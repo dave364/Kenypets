6a7ca70bde814b9dc08b695c3e027247cd35ef25
dave364 <castrodavid9872@gmail.com>
2025-07-10 13:32:15 -0300

    navbarmobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,55 @@
-import React from "react";
-import logo from "../assets/logo.jpeg";
+import React, { useState } from "react";
+import logo from "../assets/logo.png";
 import "../styles/Navbar.css";
 
-const Navbar = () => (
-  <nav className="navbar">
-    <div className="navbar__logoArea">
-      <img src={logo} alt="Kenypets logo" />
-      <span className="navbar__brand">KENYPETS</span>
-    </div>
-    <ul className="navbar__menu">
-      <li><a href="#home">Home</a></li>
-      <li><a href="#about">About</a></li>
-      <li><a href="#products">Products</a></li>
-      <li><a href="#contact">Contact</a></li>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  return (
+    <nav className="navbar">
+  <div className="navbar__logo">
+    <img src={logo} alt="Kenypets Logo" />
+  </div>
+
+  {/* Enlaces visibles en escritorio */}
+  <div className="navbar__links">
+    <a href="#home">Inicio</a>
+    <a href="#about">Productos</a>
+    <a href="#products">Quiénes Somos</a>
+    <a href="#contact">Contacto</a>
+    <a href="#politic">Políticas de devolución</a>
+  </div>
+
+      {/* Sidebar (menú lateral para mobile) */}
+<div
+  className={`navbar__sidebar ${menuOpen ? "open" : ""}`}
+  onClick={() => setMenuOpen(false)}
+>
+  <div
+    className="navbar__sidebar-content"
+    onClick={(e) => e.stopPropagation()}>
+        <div className="sidebar__logo">
+    <img src={logo} alt="Kenypets Logo" />
+  </div>
+    <a href="#home" onClick={() => setMenuOpen(false)}>Inicio</a>
+    <a href="#about" onClick={() => setMenuOpen(false)}>Productos</a>
+    <a href="#products" onClick={() => setMenuOpen(false)}>Quiénes Somos</a>
+    <a href="#contact" onClick={() => setMenuOpen(false)}>Contacto</a>
+    <a href="#politic" onClick={() => setMenuOpen(false)}>Políticas de devolución</a>
+  </div>
+</div>
+
+      {/* Toggle hamburguesa */}
+      <div
+        className={`navbar__toggle ${menuOpen ? "open" : ""}`}
+        onClick={() => setMenuOpen(!menuOpen)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
