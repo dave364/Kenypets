206cf559c44dbf2472ab09bff643ecf785dec991
dave364 <castrodavid9872@gmail.com>
2026-02-08 14:21:40 -0300

    navbarmobile2

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,39 +7,8 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-  <div className="navbar__logo">
-    <img src={logo} alt="Kenypets Logo" />
-  </div>
 
-  {/* Enlaces visibles en escritorio */}
-  <div className="navbar__links">
-    <a href="#home">Inicio</a>
-    <a href="#about">Productos</a>
-    <a href="#products">Quiénes Somos</a>
-    <a href="#contact">Contacto</a>
-    <a href="#politic">Políticas de devolución</a>
-  </div>
-
-      {/* Sidebar (menú lateral para mobile) */}
-<div
-  className={`navbar__sidebar ${menuOpen ? "open" : ""}`}
-  onClick={() => setMenuOpen(false)}
->
-  <div
-    className="navbar__sidebar-content"
-    onClick={(e) => e.stopPropagation()}>
-        <div className="sidebar__logo">
-    <img src={logo} alt="Kenypets Logo" />
-  </div>
-    <a href="#home" onClick={() => setMenuOpen(false)}>Inicio</a>
-    <a href="#about" onClick={() => setMenuOpen(false)}>Productos</a>
-    <a href="#products" onClick={() => setMenuOpen(false)}>Quiénes Somos</a>
-    <a href="#contact" onClick={() => setMenuOpen(false)}>Contacto</a>
-    <a href="#politic" onClick={() => setMenuOpen(false)}>Políticas de devolución</a>
-  </div>
-</div>
-
-      {/* Toggle hamburguesa */}
+      {/* ☰ Hamburguesa izquierda */}
       <div
         className={`navbar__toggle ${menuOpen ? "open" : ""}`}
         onClick={() => setMenuOpen(!menuOpen)}
@@ -48,6 +17,42 @@ const Navbar = () => {
         <span></span>
         <span></span>
       </div>
+
+      {/* 🐾 Logo derecha */}
+      <div className="navbar__logo">
+        <img src={logo} alt="Kenypets Logo" />
+      </div>
+
+      {/* Desktop */}
+      <div className="navbar__links">
+        <a href="#home">Inicio</a>
+        <a href="#about">Productos</a>
+        <a href="#products">Quiénes Somos</a>
+        <a href="#contact">Contacto</a>
+        <a href="#politic">Políticas de devolución</a>
+      </div>
+
+      {/* Sidebar */}
+      <div
+        className={`navbar__sidebar ${menuOpen ? "open" : ""}`}
+        onClick={() => setMenuOpen(false)}
+      >
+        <div
+          className="navbar__sidebar-content"
+          onClick={(e) => e.stopPropagation()}
+        >
+          <div className="sidebar__logo">
+            <img src={logo} alt="Kenypets Logo" />
+          </div>
+
+          <a onClick={() => setMenuOpen(false)} href="#home">Inicio</a>
+          <a onClick={() => setMenuOpen(false)} href="#about">Productos</a>
+          <a onClick={() => setMenuOpen(false)} href="#products">Quiénes Somos</a>
+          <a onClick={() => setMenuOpen(false)} href="#contact">Contacto</a>
+          <a onClick={() => setMenuOpen(false)} href="#politic">Políticas de devolución</a>
+        </div>
+      </div>
+
     </nav>
   );
 };
