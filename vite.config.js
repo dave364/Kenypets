ed2459caa8fd59c890e18829c47b1d755b2d6045
David Castro <110833807+dave364@users.noreply.github.com>
2026-08-31 09:31:45 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  cacheDir: '.vite-cache'
+    plugins: [react()],
+    base: '/Kenypets/',
+    cacheDir: '.vite-cache'
 })
